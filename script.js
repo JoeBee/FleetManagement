7b5b8ad495e8832b9580a83cd3ce3be13fc1574f
Joe B <Joe@bcjj.com>
2024-03-25 19:30:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ---------------------------------------
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// ---------------------------------------
+// *** INPUT TAB ************
+
+function runScript() {
+  // Example script - replace with your own functionality
+  alert("Script executed!");
+}
+
+function updateRowsCount() {
+  var textArea = document.getElementById("dataTextArea");
+  var rowCount = textArea.value.split("\n").length;
+  document.getElementById("message").textContent = "Total Rows: " + rowCount;
+}
+
+// ---------------------------------------
